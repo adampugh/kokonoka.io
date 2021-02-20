f4c093769d4956c38c9051d684f314c5cbb14c5d
Adam <adampugh@zoho.com>
2021-02-20 22:30:43 +0000

    fixed issue with navigation scroll w/ multiple clicks

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -3,42 +3,50 @@ const t1 = new TimelineMax({ paused: true });
 t1.to('.navigation__toggle-btn__one', 0.6, {
     y: 6,
     rotation: 45,
-    ease: Expo.easeInOut
+    ease: Expo.easeInOut,
 });
 
 t1.to('.navigation__toggle-btn__two', 0.6, {
     y: -6,
     rotation: -45,
     ease: Expo.easeInOut,
-    delay: -0.6
+    delay: -0.6,
 });
 
 t1.to('.navigation__menu', 1.2, {
     top: '0%',
     ease: Expo.easeInOut,
-    delay: -1.2
+    delay: -1.2,
 });
 
-t1.staggerFrom('.navigation__menu ul li', 1.2, {
-    x: -200,
-    opacity: 0,
-    ease: Expo.easeOut
-}, 0.1);
+t1.staggerFrom(
+    '.navigation__menu ul li',
+    1.2,
+    {
+        x: -200,
+        opacity: 0,
+        ease: Expo.easeOut,
+    },
+    0.1
+);
 
 const reverseColour = () => {
     const whiteRgb = 'rgb(255, 255, 255)';
     const $navigation = $('.navigation');
     const navigationColour = $navigation.css('color');
     const $navigationToggleBtns = $('.navigation__toggle-btn__one, .navigation__toggle-btn__two');
-    
+    const $body = $('body');
+
     if (navigationColour === whiteRgb) {
-        $navigationToggleBtns.delay(2000).animate({ backgroundColor: '#101010'}, 'slow');
-        $navigation.delay(2000).animate({ color: '#101010'}, 'slow')
+        $body.css('overflow-y', 'scroll');
+        $navigationToggleBtns.delay(2000).animate({ backgroundColor: '#101010' }, 'slow');
+        $navigation.delay(2000).animate({ color: '#101010' }, 'slow');
     } else {
-        $navigationToggleBtns.animate({ backgroundColor: '#fff'}, 'slow');
-        $navigation.animate({ color: '#fff'}, 'slow')
+        $body.css('overflow-y', 'hidden');
+        $navigationToggleBtns.animate({ backgroundColor: '#fff' }, 'slow');
+        $navigation.animate({ color: '#fff' }, 'slow');
     }
-}
+};
 
 t1.reverse();
 
@@ -50,8 +58,14 @@ const reverseColourAndAnimation = () => {
     t1.reversed(!t1.reversed());
 };
 
+const disableClickDuringAnimation = ($el, duration, fn) => {
+    $el.off();
+    fn();
+    setTimeout(() => $el.on('click', () => disableClickDuringAnimation($el, duration, fn)), duration);
+};
+
 $navigationToggleBtn.on('click', () => {
-    reverseColourAndAnimation();
+    disableClickDuringAnimation($navigationToggleBtn, 2000, reverseColourAndAnimation);
 });
 
 $navigationMenuLinks.on('click', () => {
@@ -67,7 +81,19 @@ const $fourthImage = $('.navigation__hover__image4');
 const images = [$firstImage, $secondImage, $thirdImage, $fourthImage];
 $.each(images, (index, image) => image.hide());
 
-$(navItems[0]).hover(() => $firstImage.fadeIn(1000), () => $firstImage.fadeOut(1000));
-$(navItems[1]).hover(() => $secondImage.fadeIn(1000), () => $secondImage.fadeOut(1000));
-$(navItems[2]).hover(() => $thirdImage.fadeIn(1000), () => $thirdImage.fadeOut(1000));
-$(navItems[3]).hover(() => $fourthImage.fadeIn(1000), () => $fourthImage.fadeOut(1000));
+$(navItems[0]).hover(
+    () => $firstImage.fadeIn(1000),
+    () => $firstImage.fadeOut(1000)
+);
+$(navItems[1]).hover(
+    () => $secondImage.fadeIn(1000),
+    () => $secondImage.fadeOut(1000)
+);
+$(navItems[2]).hover(
+    () => $thirdImage.fadeIn(1000),
+    () => $thirdImage.fadeOut(1000)
+);
+$(navItems[3]).hover(
+    () => $fourthImage.fadeIn(1000),
+    () => $fourthImage.fadeOut(1000)
+);
